3e2d5ac1991d58c47c6005c0ee2b08f7d7aafeba
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2024-01-08 16:58:46 +0300

    add .venv and code project DZ_2

--- DZ_2/task1.py
+++ DZ_2/task1.py
@@ -1,15 +1,25 @@
+'''
+Создать страницу, на которой будет форма для ввода имени и электронной почты, 
+при отправке которой будет создан cookie-файл с данными пользователя, а 
+также будет произведено перенаправление на страницу приветствия, 
+где будет отображаться имя пользователя.
+На странице приветствия должна быть кнопка «Выйти», при нажатии на которую 
+будет удалён cookie-файл с данными пользователя и произведено перенаправление на 
+страницу ввода имени и электронной почты.
+'''
+
 from flask import Flask, render_template, request, make_response, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/welcome')
 def welcome():
     username = request.cookies.get('username')
-    return render_template('welcome.html', username=username)
+    return render_template('index1.html', username=username)
 
 @app.route('/logout')
 def logout():
