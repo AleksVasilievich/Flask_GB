1e6044919b9da2ea76e2c71fd0f57c3aad82d49a
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2024-01-08 15:35:41 +0300

    add DZ_2

---
+++ DZ_2/task1.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/welcome')
+def welcome():
+    username = request.cookies.get('username')
+    return render_template('welcome.html', username=username)
+
+@app.route('/logout')
+def logout():
+    resp = make_response(redirect(url_for('index')))
+    resp.set_cookie('username', '', expires=0)
+    return resp
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form['name']
+    email = request.form['email']
+    resp = make_response(redirect(url_for('welcome')))
+    resp.set_cookie('username', username)
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
