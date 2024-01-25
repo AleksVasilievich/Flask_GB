bb6af228783994de594642234932430fca03d1f4
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2024-01-25 19:54:15 +0300

    add DZ_3

--- DZ_3/app_3.py
+++ DZ_3/app_3.py
@@ -1,16 +1,37 @@
-from flask import render_template, request, Flask
+from hashlib import sha256
+
+from flask import render_template, request, Flask, redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 
-from forms_3 import LoginForm
-
+from DZ_3.templates.models_3 import db, User
+from forms_3 import LoginForm, RegistrationForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'3c22c2862425a412a0e27ed0951638a83de0840a8f34a788984f9ca75a354af7'
 csrf = CSRFProtect(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../instance/mydatabase.db'
+db.init_app(app)
+
+@app.cli.command('init-db')
+def init_db():
+    db.create_all()
+    print('ok')
 
 @app.route('/')
 def index():
     return 'Hi!'
+
+@app.route('/forms/', methods=['GET', 'POST'])
+def forms():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=sha256(form.password.data.encode(enkoding='utf-8')).hexdigest())
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template(template_name_or_list='user.html', form=form)
 
 @app.route('/main/')
 def main():
--- DZ_3/forms_3.py
+++ DZ_3/forms_3.py
@@ -1,20 +1,37 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, IntegerField, SelectField
+# from wtforms.validators import DataRequired, EqualTo, Email, Length
+#
+#
+# class LoginForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#
+#
+# class RegisterForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired()])
+#     age = IntegerField('Age', validators=[DataRequired()])
+#     gender = SelectField('Gender', choices=[('male', 'Мужчина'), ('female', 'Женщина')])
+#
+# class RegistrationForm(FlaskForm):
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField, SelectField
 from wtforms.validators import DataRequired, EqualTo, Email, Length
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = StringField('Submit')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-
-
-class RegisterForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
-    gender = SelectField('Gender', choices=[('male', 'Мужчина'), ('female', 'Женщина')])
-
-class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-
