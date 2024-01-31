8bc258d00d5333834438b9db6db1dd8d490b2811
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2024-01-31 19:47:16 +0300

    Add DZ_4 File

---
+++ DZ_4/asunc.py
@@ -0,0 +1,33 @@
+import requests
+import asyncio
+import aiohttp
+import time
+import sys
+
+async def download_image(url):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    filename = "DZ_4/foto/" + url.split('/')[-1]
+                    with open(filename, 'wb') as f:
+                        f.write(await response.read())
+                    print(f"Изображение {filename} успешно загружено")
+                else:
+                    print(f"Не удалось загрузить изображение с URL: {url}")
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения с URL: {url}, {e}")
+
+async def main_asunc(urls):
+    start_time = time.time()
+    tasks = [download_image(url) for url in urls]
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    print(f"Общее время выполнения программы: {end_time - start_time} секунд")
+
+if __name__ == "__main__":
+    urls = sys.argv[1:]
+    if not urls:
+        print("Укажите список URL-адресов через аргументы командной строки")
+    else:
+        asyncio.run(main_asunc(urls))
---
+++ DZ_4/proc.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from multiprocessing.dummy import Pool as ThreadPool
+import time
+import sys
+
+def download_image(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            filename = "DZ_4/foto/" + url.split('/')[-1]
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            print(f"Изображение {filename} успешно загружено")
+        else:
+            print(f"Не удалось загрузить изображение с URL: {url}")
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения с URL: {url}, {e}")
+
+def main_proc(urls):
+    start_time = time.time()
+    pool = ThreadPool()
+    pool.map(download_image, urls)
+    pool.close()
+    pool.join()
+    end_time = time.time()
+    print(f"Общее время выполнения программы: {end_time - start_time} секунд")
+
+if __name__ == "__main__":
+    urls = sys.argv[1:]
+    if not urls:
+        print("Укажите список URL-адресов через аргументы командной строки")
+    else:
+        main_proc(urls)
